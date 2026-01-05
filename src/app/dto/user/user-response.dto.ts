7adecc98d06ebc97281422db337bda3117d76bf5
thshurka <114878213+thshurka@users.noreply.github.com>
2026-01-06 00:01:03 +0500

    feat: создание DTO ответа User

---
+++ src/app/dto/user/user-response.dto.ts
@@ -0,0 +1,27 @@
+import { Expose } from 'class-transformer';
+
+/**
+ * DTO для ответа пользователя
+ */
+export class UserResponseDto {
+  @Expose()
+  id: string;
+
+  @Expose()
+  name: string;
+
+  @Expose()
+  email: string;
+
+  @Expose()
+  avatar?: string;
+
+  @Expose()
+  type: 'pro' | 'normal';
+
+  @Expose()
+  createdAt: string;
+
+  @Expose()
+  updatedAt: string;
+}
